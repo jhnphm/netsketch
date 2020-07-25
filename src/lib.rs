f7ecc971890278821a1659e361303e25e6757ba9
John Pham <jhnphm@gmail.com>
2020-07-25 15:22:57 -0400

    properly handle roomid parsing errors with and_then

--- src/lib.rs
+++ src/lib.rs
@@ -8,35 +8,80 @@ use tokio::sync::{mpsc, RwLock};
 use std::vec::Vec;
 use std::str;
 
+pub type UserId = usize;
+pub type UserName = String;
 
+/// Our global unique user id counter.
+static NEXT_USERID: AtomicUsize = AtomicUsize::new(1);
 
-pub struct User {
-    pub name: String 
-    // todo- password? 
-}
 
 pub struct Connection {
-    pub user: User,
-    pub sender: mpsc::UnboundedSender<Result<WsMessage, warp::Error>>
+    username: String,
+    userid: UserId,
+    tx_conn: mpsc::UnboundedSender<Result<WsMessage, warp::Error>>,
 }
 
-
 pub struct Message {
-    user: User,
     message: String
 }
 
 #[derive(Default)]
 pub struct Room {
-    pub connections: RwLock<HashMap<usize, Connection>>,
-    chat_messages: RwLock<Vec<Message>>,
+    connections: RwLock<HashMap<UserId, Connection>>,
+    chat_messages: RwLock<Vec<(UserName,Message)>>,
     canvas: RwLock<Vec<Layer>>
+}
+impl Room {
+    pub async fn connect(&self, tx_conn: mpsc::UnboundedSender<Result<WsMessage, warp::Error>>, username: String) -> UserId{
+        // Use a counter to assign a new unique ID for this user.
+        let userid = NEXT_USERID.fetch_add(1, Ordering::Relaxed);
+
+        eprintln!("New chat user: {} {}", userid, username);
+        let connection = Connection {username, userid, tx_conn};
+
+        // Save the sender in our list of connected users.
+        self.connections.write().await.insert(userid, connection);
+
+        userid
+    }
+
+    pub async fn receive_msg(&self, userid: UserId, msg: WsMessage){
+        // // Skip any non-Text messages...
+        // let msg = if let Ok(s) = msg.to_str() {
+        //     s
+        // } else {
+        //     return;
+        // };
+
+        // let new_msg = format!("<User#{}>: {}", my_id, msg);
+
+        // // New message from this user, send it to everyone else (except same uid)...
+        // for (&uid, tx) in users.read().await.iter() {
+        //     if my_id != uid {
+        //         if let Err(_disconnected) = tx.send(Ok(Message::text(new_msg.clone()))) {
+        //             // The tx is disconnected, our `user_disconnected` code
+        //             // should be happening in another task, nothing more to
+        //             // do here.
+        //         }
+        //     }
+        // }
+    }
+
+    pub async fn disconnect (&self, userid: UserId){
+        // Stream closed up, so remove from the user list
+        let mut conn_map = self.connections.write().await;
+        if let Some(conn) = conn_map.get(&userid){
+            eprintln!("good bye user: {} {}", userid, conn.username);
+            conn_map.remove(&userid);
+        }
+
+    }
 }
 
 
 
 struct Layer {
-    paint_strokes: Vec<PaintStroke>,
+    paint_strokes: Vec<(UserId,PaintStroke)>,
     tiles: HashMap<Coord, Tile>
 }
 
@@ -76,12 +121,6 @@ struct StrokePoint {
 }
 
 struct PaintStroke{
-    userid: usize,
     brush: Brush,
     points: Vec<StrokePoint>
 }
-
-
-
-
-
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,16 @@
-// #![deny(warnings)]
-use std::collections::HashMap;
-use std::sync::{
-    atomic::{AtomicUsize, Ordering},
-    Arc,
-};
-
-use futures::{FutureExt, StreamExt};
-use tokio::sync::{mpsc, RwLock};
-use warp::ws::{Message, WebSocket};
-use warp::Filter;
-use std::str::FromStr;
+#![deny(warnings)]
+use std::sync::Arc;
 use std::net::SocketAddr;
+use std::str::FromStr;
 use std::vec::Vec;
+use futures::{FutureExt, StreamExt};
+use tokio::sync::mpsc;
+use warp::Filter;
+use warp::ws::WebSocket;
 
 use netsketch::*;
 
 
-/// Our global unique user id counter.
-static NEXT_USER_ID: AtomicUsize = AtomicUsize::new(1);
 
 const NUM_ROOMS: usize = 100;
 
@@ -38,18 +31,17 @@ async fn main() {
     // GET /chat -> websocket upgrade
     let chat = warp::path("chat")
         .and(warp::path::param())
-        .and(warp::path::param())
-        // The `ws()` filter will prepare Websocket handshake...
-        .and(warp::ws())
         .and(rooms)
-        .map(|room_id: usize, username: String, ws: warp::ws::Ws, rooms: Arc<Vec<Arc<Room>>>| {
-            let room =  match rooms.get(room_id) {
-                Some(x) => Some(x.clone()),
-                None => None
-            };
-            // This will call our function if the handshake succeeds.
-            ws.on_upgrade(move |socket| connected(socket, room, username))
-            
+        .and_then(|room_id: usize, rooms: Arc<Vec<Arc<Room>>>| async move{
+            match rooms.get(room_id) {
+                Some(x) => Ok(x.clone()),
+                None => Err(warp::reject::not_found())
+            } 
+        })
+        .and(warp::path::param())
+        .and(warp::ws())
+        .map(| room :Arc<Room>, username: String, ws: warp::ws::Ws|    {
+            ws.on_upgrade(move |socket| connected(socket, room.clone(), username))
         });
 
 
@@ -61,86 +53,44 @@ async fn main() {
 
 }
 
-async fn fail_connected (){
-}
 
-async fn connected(ws: WebSocket, room: Option<Arc<Room>>, username: String) {
-    if let Some(room) = room {
 
-        // Use a counter to assign a new unique ID for this user.
-        let my_id = NEXT_USER_ID.fetch_add(1, Ordering::Relaxed);
+async fn connected(ws: WebSocket, room: Arc<Room>, username: String) {
 
-        eprintln!("New chat user: {} {}", my_id, username);
 
-        // Split the socket into a sender and receive of messages.
-        let (ws_tx, mut ws_rx) = ws.split();
+    // Split the socket into a sender and receive of messages.
+    let (ws_tx, mut ws_rx) = ws.split();
 
-        // Use an unbounded channel to handle buffering and flushing of messages
-        // to the websocket...
-        let (tx, rx) = mpsc::unbounded_channel();
-        tokio::task::spawn(rx.forward(ws_tx).map(|result| {
-            if let Err(e) = result {
-                eprintln!("websocket send error: {}", e);
+    // Use an unbounded channel to handle buffering and flushing of messages
+    // to the websocket...
+    let (tx, rx) = mpsc::unbounded_channel();
+    tokio::task::spawn(rx.forward(ws_tx).map(|result| {
+        if let Err(e) = result {
+            eprintln!("websocket send error: {}", e);
+        }
+    }));
+
+
+    let userid = room.connect(tx, username).await;
+
+
+    // Every time the user sends a message, broadcast it to
+    // all other users...
+    while let Some(result) = ws_rx.next().await {
+        let msg = match result {
+            Ok(msg) => msg,
+            Err(e) => {
+                eprintln!("websocket error(uid={}): {}", userid, e);
+                break;
             }
-        }));
-
-        let connection = Connection {user: User {name: username}, sender: tx};
-
-        // Save the sender in our list of connected users.
-        room.connections.write().await.insert(my_id, connection);
-//
-//    // Return a `Future` that is basically a state machine managing
-//    // this specific user's connection.
-//
-//    // Make an extra clone to give to our disconnection handler...
-//    let users2 = users.clone();
-//
-//    // Every time the user sends a message, broadcast it to
-//    // all other users...
-//    while let Some(result) = ws_rx.next().await {
-//        let msg = match result {
-//            Ok(msg) => msg,
-//            Err(e) => {
-//                eprintln!("websocket error(uid={}): {}", my_id, e);
-//                break;
-//            }
-//        };
-//        user_message(my_id, msg, &users).await;
-//    }
-//
-//    // user_ws_rx stream will keep processing as long as the user stays
-//    // connected. Once they disconnect, then...
-//    user_disconnected(my_id, &users2).await;
-
+        };
+        room.receive_msg(userid, msg).await;
     }
+
+
+//    // ws_rx stream will keep processing as long as the user stays
+//    // connected. Once they disconnect, then...
+    room.disconnect(userid).await;
+
 }
 
-//async fn user_message(my_id: usize, msg: Message, users: &Users) {
-//    // Skip any non-Text messages...
-//    let msg = if let Ok(s) = msg.to_str() {
-//        s
-//    } else {
-//        return;
-//    };
-//
-//    let new_msg = format!("<User#{}>: {}", my_id, msg);
-//
-//    // New message from this user, send it to everyone else (except same uid)...
-//    for (&uid, tx) in users.read().await.iter() {
-//        if my_id != uid {
-//            if let Err(_disconnected) = tx.send(Ok(Message::text(new_msg.clone()))) {
-//                // The tx is disconnected, our `user_disconnected` code
-//                // should be happening in another task, nothing more to
-//                // do here.
-//            }
-//        }
-//    }
-//}
-//
-//async fn user_disconnected(my_id: usize, users: &Users) {
-//    eprintln!("good bye user: {}", my_id);
-//
-//    // Stream closed up, so remove from the user list
-//    users.write().await.remove(&my_id);
-//}
-//
